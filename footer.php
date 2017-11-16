a723ae9f0e476d3042d05e7169d88731aa9e925a
DJ <dmitri.jurovski@gmail.com>
2017-11-16 21:53:52 +0200

    Added Footer & Header Modify

--- footer.php
+++ footer.php
@@ -1,8 +1,66 @@
 
-    
+
   </div> <!-- /#mainWraaper -->
-      <footer>
-        <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
+
+  <!-- Footer -->
+  <footer class="bg-dark text-light" >
+  <div class="container">
+  <div class="row">
+    <div class="col-6 col-lg-3">
+      <h4>Information</h4>
+      <ul class="navbar">
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+
+    </div>
+    <div class="col-6 col-lg-3">
+      <h4>My account</h4>
+    </div>
+    <div class="col-6 col-lg-3">
+      <h4>Follow us</h4>
+
+      <a href="#" class="facebook text-light"><span class="fa-stack fa-lg">
+          <i class="fa fa-circle fa-stack-2x text-blue"></i>
+          <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+      </span>Facebook</a><br>
+
+      <a href="#" class="twitter text-light"><span class="fa-stack fa-lg">
+          <i class="fa fa-circle fa-stack-2x text-blue"></i>
+          <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+      </span>Twitter</a><br>
+
+      <a href="#" class="rss text-light"><span class="fa-stack fa-lg">
+          <i class="fa fa-circle fa-stack-2x text-blue"></i>
+          <i class="fa fa-rss fa-stack-1x fa-inverse"></i>
+      </span>Rss</a>
+
+    </div>
+
+    <div class="col-6 col-lg-3">
+      <h4>Contact us</h4>
+      <address>
+        <strong>Twitter, Inc.</strong><br />
+        1355 Market St, Suite 900<br />
+        San Francisco, CA 94103<br />
+        <abbr title="Phone">P:</abbr> (123) 456-7890
+      </address>
+
+      <address>
+        <strong>Full Name</strong><br />
+        <a href="mailto:#">first.last@example.com</a>
+      </address>
+    </div>
+
+  </div>
+  <hr class="border-success">
+
+  <p class="text-success" ><small>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> by iWeb</small></p>
+</div>
+
+
+
       </footer>
 
 
