0c611a60486f009df2567897e94881938107e937
pierre <support@iweb.ee>
2017-11-22 20:18:26 +0200

    6 products per row

--- functions.php
+++ functions.php
@@ -333,12 +333,12 @@ function widget( $args, $instance ) {
  *
  */
 
-add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 6 );
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 
 function new_loop_shop_per_page( $cols ) {
   // $cols contains the current number of products per page based on the value stored on Options -> Reading
   // Return the number of products you wanna show per page.
-  $cols = 40;
+  $cols = 6;
   return $cols;
 }
 
