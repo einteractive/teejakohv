f176295722c0976adcbf1f632709e8dd96a982dd
pierre <support@iweb.ee>
2017-11-23 00:30:17 +0200

    Sidebar Bug Fix

---
+++ sidebar-shop.php
@@ -0,0 +1,10 @@
+<div class="shop-sidebar">
+
+	<?php if ( ! dynamic_sidebar( 'shop-sidebar' ) ): ?>
+
+	<h3>Sidebar Setup</h3>
+	<p>Please add widgets to the blog sidebar to have them display here.</p>	
+
+	<?php endif; ?>
+	
+</div>
--- sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! dynamic_sidebar( 'page-sidebar' ) ): ?>
-
-	<h3>Sidebar Setup</h3>
-	<p>Please add widgets to the page sidebar to have them display here.</p>	
-
-	<?php endif; ?>
