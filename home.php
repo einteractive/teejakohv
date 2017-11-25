7c9b6e8f3dc762757482ac73b156cf1072f91310
DJ <dmitri.jurovski@gmail.com>
2017-11-25 23:24:19 +0200

    Blog markup

--- home.php
+++ home.php
@@ -1,31 +1,37 @@
 <?php get_header(); ?>
+<!-- Main .container Start-->
+ <div class="container">
+    <div class="row row-offcanvas row-offcanvas-left pt-3">
 
-  <div class="container">   
-    <div class="row">
-      
-      <div class="col-md-9">
 
-        <div class="page-header">
-          <h1><?php wp_title(''); ?></h1>
-        </div>
+        <div id="pageRowSideBar" class="col-5 col-lg-3 sidebar-offcanvas" id="sidebar">
+					<?php get_sidebar(); ?>
+        </div><!--/span-->
+
+        <div id="pageRowMain" class="col-12 col-lg-9">
+
+            <button type="button" class="btn btn-success btn-sm d-lg-none" data-toggle="offcanvas"><i class="fa fa-hand-o-up" aria-hidden="true"></i> Shop Category</button>
+
+
+
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
           <article class="post">
             <?php
-                $thumbnail_id = get_post_thumbnail_id(); 
+                $thumbnail_id = get_post_thumbnail_id();
                 $thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
-                $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);                
+                $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);
               ?>
               <a href="<?php the_permalink(); ?>"><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>"></a>
 
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <p><em>
-              By <?php the_author(); ?> 
+              By <?php the_author(); ?>
               on <?php echo the_time('l, F jS, Y');?>
               in <?php the_category( ', ' ); ?>.
               <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
-            </em></p>            
+            </em></p>
 
             <?php the_excerpt(); ?>
 
@@ -33,9 +39,9 @@
 
           </article>
 
-         
+
         <?php endwhile; else: ?>
-          
+
           <div class="page-header">
             <h1>Oh no!</h1>
           </div>
@@ -44,11 +50,9 @@
 
         <?php endif; ?>
 
+      </div><!--/row-->
 
-      </div>
-      
-      <?php get_sidebar( 'blog' ); ?>
+      </div> <!-- /.container -->
 
-    </div>
 
 <?php get_footer(); ?>
