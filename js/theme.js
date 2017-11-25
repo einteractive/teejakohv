fe6399d48ae5fb6cd8546157fa095de5c75e8af8
DJ <dmitri.jurovski@gmail.com>
2017-11-25 23:19:16 +0200

    Onsale, border img

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,5 @@
   // This code add and remove to woo tabs bootstrap4 active class to anchor element
   $( "li.nav-item.active a" ).addClass( "active" );
-  //$( "li.nav-item:not(.active) a" ).removeClass( "active" );
-
   $( 'li.nav-item a' ).click(function(e){
     $( 'li.nav-item a' ).removeClass("active"); // remove class from other tab
     $(this).addClass("active"); // set class to current clicked tab
