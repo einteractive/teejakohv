75952085641f3a40926974077acf1b6a2ffd91e6
DJ <dmitri.jurovski@gmail.com>
2017-11-27 00:57:09 +0200

    Added ajax function in mini cart

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,17 @@
+"use strict";
+
+// This code add and remove to woo tabs bootstrap4 active class to anchor element
+$( "li.nav-item.active a" ).addClass( "active" );
+$( 'li.nav-item a' ).click(function(e){
+  "use strict";
+  $( 'li.nav-item a' ).removeClass("active"); // remove class from other tab
+  $(this).addClass("active"); // set class to current clicked tab
+
+});
+// Close bs4 tabs
 
 
-  // This code add and remove to woo tabs bootstrap4 active class to anchor element
-  $( "li.nav-item.active a" ).addClass( "active" );
-  $( 'li.nav-item a' ).click(function(e){
-    "use strict";
-    $( 'li.nav-item a' ).removeClass("active"); // remove class from other tab
-    $(this).addClass("active"); // set class to current clicked tab
-
-  });
-
-
-
-
+// Mini Cart button open Woo widget hover or click function
 // $('#cart').on('mouseenter', function () {
 //   $(".box").slideDown(600);
 //   console.log("IN");
@@ -21,18 +21,32 @@
 //   console.log("OUT");
 //   });
 
-
-
-  $("#cart").click(function(){
-    $(".box").slideToggle(600);
+$("#cart").click(function(){
+  $(".box").slideToggle(600);
 });
+// Close Mini Cart
 
 
-  // Off-Canvas Menu in #mainWraaper
-  $(function () {
-    'use strict'
+// Off-Canvas Menu in #mainWraaper
+$(function () {
+  'use strict'
 
-    $('[data-toggle="offcanvas"]').on('click', function () {
-      $('.row-offcanvas').toggleClass('active')
-    })
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.row-offcanvas').toggleClass('active')
   })
+})
+// Close Off-Canvas menu
+
+
+// Define the PHP function to call from here
+var data = {
+  'action': 'mode_theme_update_mini_cart'
+};
+$.post(
+  woocommerce_params.ajax_url, // The AJAX URL
+  data, // Send our PHP function
+  function(response){
+    $('#mode-mini-cart').html(response); // Repopulate the specific element with the new content
+  }
+);
+// Close anon function.
