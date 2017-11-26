964a2bbcf5991adbfd3664b847b0f4ae5a5109d5
DJ <dmitri.jurovski@gmail.com>
2017-11-27 00:37:46 +0200

    Styling mini cart

--- js/itheme.js
+++ js/itheme.js
@@ -3852,13 +3852,34 @@ return exports;
 }({},$,Popper));
 //# sourceMappingURL=bootstrap.js.map
 
+
+
   // This code add and remove to woo tabs bootstrap4 active class to anchor element
   $( "li.nav-item.active a" ).addClass( "active" );
   $( 'li.nav-item a' ).click(function(e){
+    "use strict";
     $( 'li.nav-item a' ).removeClass("active"); // remove class from other tab
     $(this).addClass("active"); // set class to current clicked tab
 
   });
+
+
+
+
+// $('#cart').on('mouseenter', function () {
+//   $(".box").slideDown(600);
+//   console.log("IN");
+//   });
+// $('#cart').on('mouseleave', function () {
+//   $(".box").slideUp(600);
+//   console.log("OUT");
+//   });
+
+
+
+  $("#cart").click(function(){
+    $(".box").slideToggle(600);
+});
 
 
   // Off-Canvas Menu in #mainWraaper
