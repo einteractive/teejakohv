936eee70e733ba098072136933138e9db2228bba
DJ <dmitri.jurovski@gmail.com>
2017-12-01 01:19:22 +0200

    Styling in footer adress, social-links

--- footer.php
+++ footer.php
@@ -17,22 +17,24 @@
     <div class="col-6 col-lg-3">
       <?php if ( ! dynamic_sidebar( 'myaccount-footer' ) ): ?> 	<?php endif; ?>
     </div>
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 social-links">
       <h3><?php _e('Follow us','teejakohv') ?></h3>
 
-      <a href="https://www.facebook.com/TEEjaKOHV/" class="facebook text-light" target="_blank"><span class="fa-stack fa-lg">
+      <a href="https://www.facebook.com/TEEjaKOHV/" class="facebook text-light" target="_blank">
+        <span class="fa-stack fa-lg">
           <i class="fa fa-circle fa-stack-2x text-blue"></i>
           <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-      </span>Facebook</a><br>
+      </span>Facebook</a>
+      <br>
 
     </div>
 
     <div class="col-6 col-lg-3">
       <h3>Contact us</h3>
       <address>
-        <strong>RC TRADING OÜ</strong><br />
-        Redise tn 8-15, <br />
-        Harjumaa, Laagri, 76401<br />
+        <strong>RC TRADING OÜ</strong><br>
+        Redise tn 8-15, <br>
+        Harjumaa, Laagri, 76401<br>
         <abbr title="Phone"><?php _e('Tel','teejakohv') ?>:</abbr> <a href="tel:+37255923710">(372) 5592-3710</a>
          <a href="mailto:info@teejakohv.ee">info@teejakohv.ee</a>
 
@@ -42,7 +44,7 @@
   </div>
   <hr class="border-success">
 
-  <p class="text-success developers-link"><small><?php bloginfo('name'); ?> <?php echo date('Y'); ?> @ <a class="developers-link" href="www.iweb.ee" target="_blank">iweb.ee</small></p>
+  <p class="text-success developers-link"><small><?php bloginfo('name'); ?> <?php echo date('Y'); ?> @ <a class="developers-link" href="www.iweb.ee" target="_blank">iweb.ee</a></small></p>
 </div>
 
 
